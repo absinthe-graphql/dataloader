db0279c4fb42bfeef331d94b7313070bba905c82
Andy McVitty <Andy.mcvitty@gmail.com>
2018-02-05 22:40:15 +0100

    Add test for :through associations

--- priv/test_repo/migrations/1_migrate_all.exs
+++ priv/test_repo/migrations/1_migrate_all.exs
@@ -9,5 +9,10 @@ defmodule Absinthe.Ecto.TestRepo.Migrations.MigrateAll do
     create table(:posts) do
       add :user_id, references(:users)
     end
+    
+    create table(:comments) do
+      add :post_id, references(:posts)
+      add :user_id, references(:users)
+    end
   end
 end
