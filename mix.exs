81de7656a89d902a29fc58eb3207275a2d054e35
Maarten van Vliet <maartenvanvliet@gmail.com>
2018-10-12 21:12:30 +0200

    Add source url in mix.exs

--- mix.exs
+++ mix.exs
@@ -12,6 +12,7 @@ defmodule Dataloader.Mixfile do
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
       aliases: aliases(),
+      source_url: "https://github.com/absinthe-graphql/dataloader",
       docs: [
         main: "Dataloader",
         source_ref: "v#{@version}",
