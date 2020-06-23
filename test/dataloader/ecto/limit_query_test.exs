ac82f0cd1dc3eee82d45094394d2d92647c33ed2
Mike Buhot <m.buhot@gmail.com>
2020-06-23 15:19:48 +1000

    Add tests for has-through and many-to-many associations with limits

--- test/dataloader/ecto/limit_query_test.exs
+++ test/dataloader/ecto/limit_query_test.exs
@@ -1,7 +1,7 @@
 defmodule Dataloader.LimitQueryTest do
   use ExUnit.Case, async: true
 
-  alias Dataloader.{User, Post}
+  alias Dataloader.{User, Post, Like, Score}
   import Ecto.Query
   alias Dataloader.TestRepo, as: Repo
 
@@ -28,6 +28,14 @@ defmodule Dataloader.LimitQueryTest do
 
     Post
     |> where([p], is_nil(p.deleted_at))
+    |> order_by(^order_by)
+    |> limit(^limit)
+  end
+
+  defp query(schema, %{limit: limit, order_by: order_by}, test_pid) do
+    send(test_pid, :querying)
+
+    schema
     |> order_by(^order_by)
     |> limit(^limit)
   end
@@ -84,5 +92,66 @@ defmodule Dataloader.LimitQueryTest do
 
     assert [post2] == Dataloader.get(loader, Test, args, user1)
     assert [post3] == Dataloader.get(loader, Test, args, user2)
+  end
+
+  test "Load has-many-through association with limit", %{loader: loader} do
+    leaderboard = %Dataloader.Leaderboard{name: "Top Bloggers"} |> Repo.insert!()
+    user1 = %User{username: "Ben Wilson", leaderboard: leaderboard} |> Repo.insert!()
+    user2 = %User{username: "Bruce Williams", leaderboard: leaderboard} |> Repo.insert!()
+
+    posts =
+      [
+        %Post{user_id: user1.id, title: "foo"},
+        %Post{user_id: user1.id, title: "baz"},
+        %Post{user_id: user2.id, title: "bar"},
+        %Post{user_id: user2.id, title: "qux"}
+      ]
+      |> Enum.map(&Repo.insert!/1)
+
+    [_score1, score2, _score3, score4] =
+      Enum.map(posts, fn post ->
+        %Score{post_id: post.id, leaderboard_id: leaderboard.id} |> Repo.insert!()
+      end)
+
+    args = {:scores, %{limit: 1, order_by: [desc: :post_id]}}
+
+    loader =
+      loader
+      |> Dataloader.load_many(Test, args, [user1, user2])
+      |> Dataloader.run()
+
+    assert [score2] == Dataloader.get(loader, Test, args, user1)
+    assert [score4] == Dataloader.get(loader, Test, args, user2)
+  end
+
+  test "Load many-to-many association with limit", %{loader: loader} do
+    user1 = %User{username: "Ben Wilson"} |> Repo.insert!()
+    user2 = %User{username: "Bruce Williams"} |> Repo.insert!()
+    user3 = %User{username: "Chris McCord"} |> Repo.insert!()
+    user4 = %User{username: "Jose Valim"} |> Repo.insert!()
+
+    [post1, post2, post3, post4] =
+      [
+        %Post{user_id: user1.id, title: "foo"},
+        %Post{user_id: user1.id, title: "baz"},
+        %Post{user_id: user2.id, title: "bar"},
+        %Post{user_id: user2.id, title: "qux"}
+      ]
+      |> Enum.map(&Repo.insert!/1)
+
+    %Like{user_id: user3.id, post_id: post1.id} |> Repo.insert!()
+    %Like{user_id: user3.id, post_id: post3.id} |> Repo.insert!()
+    %Like{user_id: user4.id, post_id: post2.id} |> Repo.insert!()
+    %Like{user_id: user4.id, post_id: post4.id} |> Repo.insert!()
+
+    args = {:liked_posts, %{limit: 1, order_by: [desc: :title]}}
+
+    loader =
+      loader
+      |> Dataloader.load_many(Test, args, [user3, user4])
+      |> Dataloader.run()
+
+    assert [post1] == Dataloader.get(loader, Test, args, user3)
+    assert [post4] == Dataloader.get(loader, Test, args, user4)
   end
 end
