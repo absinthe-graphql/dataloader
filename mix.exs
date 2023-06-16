1cea4348d4a44293e8f76143b175cd9e7aa1edd7
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2023-06-17 00:41:12 +0100

    Bump dialyxir to prevent compilation warnings

--- mix.exs
+++ mix.exs
@@ -65,7 +65,7 @@ defmodule Dataloader.Mixfile do
       {:opentelemetry_process_propagator, "~> 0.2.1", optional: true},
       {:ecto_sql, "~> 3.0", optional: true, only: :test},
       {:postgrex, "~> 0.14", only: :test, runtime: false},
-      {:dialyxir, "~> 1.0.0", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.3.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false}
     ]
   end
