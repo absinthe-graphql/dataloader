adb6594fd5712b7dee24ea82561cf8ae68aae3cb
Kian-Meng Ang <kianmeng@cpan.org>
2023-11-13 13:43:33 +0800

    Bump apps version in GitHub CI
    List of changes:
    - bump actions/checkout to v4
    - remove unused deps
    - support Erlang/OTP 26.x and Elixir 1.15.x
    - update docker command to work with podman
    - use default naming of jobs to show `lint` flag

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Dataloader.Mixfile do
     [
       app: :dataloader,
       version: @version,
-      elixir: "~> 1.12",
+      elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       package: package(),
